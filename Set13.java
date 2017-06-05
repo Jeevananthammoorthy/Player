c419f95a322ecb01a19c77c072b50624e4b1c1a8
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2017-06-05 06:10:24 -0700

    Create Set13.java

---
+++ Set13.java
@@ -0,0 +1,10 @@
+import java.util.*;
+ class Set13{
+     public static void main(String args[])
+     {
+         Scanner in=new Scanner(System.in);
+         int str=in.nextInt();
+         StringBuffer sb=new StringBuffer(str);
+         System.out.println(sb.reverse());
+     }
+ }
