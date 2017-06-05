953c5babbc2cb8af6f9b244399d9fa1838c5e7c0
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2017-06-05 05:48:29 -0700

    Create Set11.java

---
+++ Set11.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Set11{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        String str=in.nextLine();
+        StringBuffer sb=new StringBuffer(str);
+        System.out.println(sb.reverse());
+    }
+}
